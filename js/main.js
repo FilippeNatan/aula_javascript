da826fa658145325b51f35dd19480f711dc90f5a
Filippe Xavier <filippe_natan@hotmail.com>
2020-06-05 15:05:38 -0300

    1° Commit
    1° Commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+alert("Meu primeiro JS");
+
+var nome = "Filippe Xavier";
+var idade =  29;
+var xp = 12;
+var frase = "Milan é o melhor time do mundo!"
+
+console.log(nome + " tem " + idade + " anos. E " + xp + " anos de experiência.")
+console.log(`${nome} tem ${idade} anos. E ${xp} anos de experiência.`)
+
+console.log(frase.replace("Milan", "Atletico-MG"))
